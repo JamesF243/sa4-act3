6ba15befe42426eef3cdef8c8c74a7263158b02c
James Finkelstein <jiflotr9@gmail.com>
2024-02-07 23:35:38 -0600

    guess num now gives hints abt higher or lower

--- guess_number.py
+++ guess_number.py
@@ -7,9 +7,12 @@ while guess != "q":
     if int(guess) == number:
         print("Congratulations! You guessed the right number.")
         break
-    else:
-        print(f"Sorry! Please try again.")
-        guess = input("\nWhat number am I thinking of? ")
+    elif int(guess) > number:
+        print(f"Sorry! You were too high, please try again.")
+        guess = input("What number am I thinking of? ")
+    else: 
+        print(f"Sorry! You were too low, please try again.")
+        guess = input("What number am I thinking of? ")
 
 if guess == "q":
     print(f"Sorry! The number was {number}.")
