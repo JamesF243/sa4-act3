cfc58b2c1f99d8e587a0de335d9ede3934b47cad
James Finkelstein <jiflotr9@gmail.com>
2024-02-07 23:32:32 -0600

    Added multiple guesses and q to quit to guess number

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,15 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
+guess = input("What number am I thinking of? ")
 
-if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+while guess != "q":
+    if int(guess) == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else:
+        print(f"Sorry! Please try again.")
+        guess = input("\nWhat number am I thinking of? ")
+
+if guess == "q":
+    print(f"Sorry! The number was {number}.")
