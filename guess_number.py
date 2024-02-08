d90585cb0e6c232a9c1542bc0051a3d8c7a6ffd0
James Finkelstein <jiflotr9@gmail.com>
2024-02-07 23:43:34 -0600

    Added limit of 3 guesses, and warns num guesses left

--- guess_number.py
+++ guess_number.py
@@ -2,14 +2,17 @@ number = 10
 
 print("I'm thinking of a number...")
 guess = input("What number am I thinking of? ")
+limited = 3
 
-while guess != "q":
+while guess != "q" and limited > 0:
     if int(guess) == number:
         print("Congratulations! You guessed the right number.")
         break
     else:
-        print(f"Sorry! Please try again.")
-        guess = input("\nWhat number am I thinking of? ")
+        limited -= 1
+        print(f"Sorry! You have", limited, "guesses left.")
+        if limited > 0:
+            guess = input("\nWhat number am I thinking of? ")
 
 if guess == "q":
     print(f"Sorry! The number was {number}.")
